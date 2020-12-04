cffa8dbb94b68e4c08953312d4b800f6be653c2b
Victor Kich <victorkich@yahoo.com.br>
2020-12-04 11:22:07 -0300

    More optimized plot

--- graph.py
+++ graph.py
@@ -4,10 +4,10 @@ import numpy as np
 
 df = pd.read_csv('output.csv', index_col=False)
 x = np.arange(0, df.shape[0])
-plt.plot(x, df['train_loss'], label="train_loss")
-plt.plot(x, df['val_loss'], label="val_loss")
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
+plt.plot(x, df['train_loss'], label='train_loss')
+plt.plot(x, df['val_loss'], label='val_loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
